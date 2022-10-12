51676ae4bc8a90eec306f9cdb7edb0a5e17ecd72
jdelcarpio23 <jdelcarpio23@gmail.com>
2022-10-12 18:51:25 -0500

    Apirest Productos

--- src/main/java/com/pet/Services/ProductoService.java
+++ src/main/java/com/pet/Services/ProductoService.java
@@ -1,6 +1,8 @@
 package com.pet.Services;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,9 +16,30 @@ public class ProductoService {
 	@Autowired
 	private IProductoRepository repop;
 	
-	public ArrayList<Producto> obtenerProductos(){
+	/*
+	 * public ArrayList<Producto> obtenerProductos(){
+	 * 
+	 * return (ArrayList<Producto>)repop.findAll(); };
+	 */
+	
+	public List<Producto> obtenerProductos(){
 		
-		return (ArrayList<Producto>)repop.findAll();
+		return repop.findAll();
+	};
+	
+	public Producto grabarProducto(Producto p){
+		
+		return repop.save(p);
+	};
+	
+	public Producto obtenerProducto(Integer codproducto) {
+		
+		return repop.getById(codproducto);
+	}
+	
+	public void eliminarProducto(Integer codproducto){
+		
+		repop.deleteById(codproducto);
 	};
 	
 	
--- src/main/java/com/pet/api/ApiProductoController.java
+++ src/main/java/com/pet/api/ApiProductoController.java
@@ -1,9 +1,15 @@
 package com.pet.api;
 
-import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,16 +17,35 @@ import com.pet.Services.ProductoService;
 import com.pet.model.Producto;
 
 @RestController
-@RequestMapping("/api/producto")
+@RequestMapping("/apirest/producto")
 public class ApiProductoController {
 	
 	@Autowired
 	private ProductoService servp;
-	
-	@GetMapping()
-	public ArrayList<Producto> obtenerProductos(){		
+
+	@PostMapping("/grabar")
+	public ResponseEntity<Producto> grabar(@RequestBody Producto producto){
 		
-		return servp.obtenerProductos();
+		return new ResponseEntity<>(servp.grabarProducto(producto), HttpStatus.CREATED);
 	}
+	
+	@GetMapping("/listar")
+	public ResponseEntity<List<Producto>> obtenerProductos(){		
+		
+		return new ResponseEntity<>(servp.obtenerProductos(),HttpStatus.OK);
+	}
+	
+	@GetMapping("/{codproducto}")
+	public ResponseEntity<Producto> buscar(@PathVariable Integer codproducto){
+		return new ResponseEntity<>(servp.obtenerProducto(codproducto),HttpStatus.OK);
+	}
+	
+	@DeleteMapping("/{codproducto}")
+	public ResponseEntity<Producto> eliminar(@PathVariable Integer codproducto){
+		servp.eliminarProducto(codproducto);
+		return new ResponseEntity<Producto>(HttpStatus.OK);
+	}
+	
+	
 
 }
