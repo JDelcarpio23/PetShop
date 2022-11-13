961553aaad469bbcfbae3f898f2b65099ee2234c
jdelcarpio23 <jdelcarpio23@gmail.com>
2022-11-13 09:29:20 -0500

    Postgres prueba 3

--- src/main/java/com/pet/model/Tipo.java
+++ src/main/java/com/pet/model/Tipo.java
@@ -3,8 +3,6 @@ package com.pet.model;
 import java.io.Serializable;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import lombok.Data;
